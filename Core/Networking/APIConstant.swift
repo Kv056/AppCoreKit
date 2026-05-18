dc84d5e116aac8aa46854f87818d40108c2cad65
Kv056 <41187928+Kv056@users.noreply.github.com>
2026-05-18 11:14:22 +0530

    Added APIConstant

---
+++ Core/Networking/APIConstant.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+enum APIConstants {
+
+    enum Paths {
+
+        static let users =
+            "/users"
+
+        static let login =
+            "/login"
+
+        static let products =
+            "/products"
+
+        static let profile =
+            "/profile"
+    }
+}
